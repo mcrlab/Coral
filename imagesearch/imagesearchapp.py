5cf00cb26c272274ad28d967df7348fd5091d426
james linnegar <james@linnegar.com>
2019-04-11 15:42:42 +0100

    Extracting find objects

--- imagesearch/imagesearchapp.py
+++ imagesearch/imagesearchapp.py
@@ -33,16 +33,13 @@ class App:
         self.stopEvent = None
         
         self.camera = cv2.VideoCapture(0)
-        self.camera.set(3, 640)
-        self.camera.set(4, 480)
+        self.camera.set(3, WIDTH)
+        self.camera.set(4, HEIGHT)
         
-
         self.engine = DetectionEngine('./mobilenet_ssd_v1_coco_quant_postprocess_edgetpu.tflite')
         self.labels = ReadLabelFile('./coco_labels.txt')
         
-        
         self.root = tki.Tk()
-#        self.root.attributes('-fullscreen', True)
         self.root.bind('<Escape>', lambda e: self.onClose())
         self.root.wm_protocol("WM_DELETE_WINDOW", self.onClose)
         self.panel = None
@@ -50,10 +47,16 @@ class App:
         self.stopEvent = threading.Event()
         self.thread = threading.Thread(target=self.videoLoop, args=())
         self.thread.start()
-
+        
+    def findObjects(self, image):
+        _, width, height, channels = self.engine.get_input_tensor_shape()
+        input = cv2.resize(image, (width, height))
+        input = input.reshape((width * height * channels))
+        results = self.engine.DetectWithInputTensor(input, top_k=5)
+        return results
+    
     def videoLoop(self):
-        try:
-            _, width, height, channels = self.engine.get_input_tensor_shape()
+        try: 
             while not self.stopEvent.is_set():
                 if not self.camera.isOpened():
                     continue
@@ -63,29 +66,33 @@ class App:
 
                 font = cv2.FONT_HERSHEY_SIMPLEX
                 self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
-                input = cv2.resize(self.frame, (width, height))
-                input = input.reshape((width * height * channels))
-                ans = self.engine.DetectWithInputTensor(input, top_k=5)
-                if ans:
-                    for obj in ans:
+                results = self.findObjects(self.frame)
+                if results:
+                    for obj in results:
                         if(obj.score > 0.5):
 
                             top_left = calculatePosition(obj.bounding_box[0])
                             bottom_right = calculatePosition(obj.bounding_box[1])
                             center_point = (int(top_left[0] + ((bottom_right[0] - top_left[0]) / 2)),
                                             int(top_left[1] + ((bottom_right[1] - top_left[1]) / 2)))
-#                            cv2.rectangle(self.frame, (top_left[0], top_left[1]), (bottom_right[0], bottom_right[1]), (0, 255, 0), 2)
+#                            cv2.rectangle(self.frame, top_left, bottom_right, (0, 255, 0), 1)
                 
                             label = self.labels[obj.label_id]
                             label_size = cv2.getTextSize(label, font, 0.5,cv2.LINE_AA)
                             label_width = label_size[0][0]
                             label_height = label_size[0][1]
                             
+#                            pointer
                             cv2.circle(self.frame, center_point, 5, (0,255,0),-1)
                             cv2.line(self.frame, (int(top_left[0] + label_width/2),top_left[1]), center_point, (0,255,0),2)
-                            cv2.rectangle(self.frame, (top_left[0]-1, top_left[1]-label_height), (top_left[0]+label_width, top_left[1]), (0,255,0),-1)
-                            cv2.putText(self.frame, label, (top_left[0]+5, top_left[1]-5), font, 0.5, (255,255,255),1,cv2.LINE_AA)
-                
+
+#                            label
+                            label_x = top_left[0] - 1
+                            label_y = top_left[1]-label_height
+                            if label_y < 0: label_y = 0
+                            cv2.rectangle(self.frame, (label_x, label_y), (label_x+label_width, label_y + label_height), (0,255,0),-1)
+                            cv2.putText(self.frame, label, (label_x+5, label_y + label_height-5), font, 0.5, (255,255,255))
+           
                 image = Image.fromarray(self.frame)     
                 image = ImageTk.PhotoImage(image)
         
@@ -99,7 +106,6 @@ class App:
                     self.panel.image = image
                         
         except Exception as e:
-            
             print("[INFO] caught a RuntimeError")
             print(e)
         finally:
